14606959b1e6494f41bd9ccf3a10c4fbc9a8ffc2
lucas <lucasst10@gmail.com>
2017-10-31 21:31:06 -0200

    tratamento excecao

--- src/main/java/magiccards/ui/controllers/MagicExpasionController.java
+++ src/main/java/magiccards/ui/controllers/MagicExpasionController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import magiccards.ui.entities.Card;
 import magiccards.ui.entities.MagicExpansion;
 import magiccards.ui.entities.Page;
 import magiccards.ui.entities.TablePage;
